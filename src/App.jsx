5d2426655c4e510b8bb1d3d38f5de9b8098399ea
Victor Laitila <victorlaitila@MacBook-Pro-som-tillhor-Victor.local>
2025-02-18 21:55:58 +0200

    Anecdote get and create with query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { useQuery } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAll } from './services/anecdote'
 
 const App = () => {
 
@@ -7,34 +9,42 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAll,
+    retry: 1,
+    refetchOnWindowFocus: false
+  })
 
-  return (
-    <div>
-      <h3>Anecdote app</h3>
-    
-      <Notification />
-      <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in the server</div>
+  }
+
+  const anecdotes = result.data
+
+  if (anecdotes) {
+    return (
+      <div>
+        <h3>Anecdote app</h3>
+      
+        <Notification />
+        <AnecdoteForm />
+      
+        {anecdotes.map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => handleVote(anecdote)}>vote</button>
+            </div>
           </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
-    </div>
-  )
+        )}
+      </div>
+    )
+  }
+  
 }
 
 export default App
--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,9 +1,23 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createNew } from '../services/anecdote'
+
 const AnecdoteForm = () => {
+  const queryClient =  useQueryClient() 
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createNew,
+    onSuccess: (newAnecdote) => {
+      console.log(newAnecdote)
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content, votes: 0 })
     console.log('new anecdote')
 }
 
@@ -12,7 +26,7 @@ const AnecdoteForm = () => {
       <h3>create new</h3>
       <form onSubmit={onCreate}>
         <input name='anecdote' />
-        <button type="submit">create</button>
+        <button type='submit'>create</button>
       </form>
     </div>
   )
